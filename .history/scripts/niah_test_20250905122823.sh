9343a8104edc3c39c59815d0f4a9970a28ece256
Zefan Cai <zefncai@gmail.com>
2025-10-03 22:23:12 -0500

    asd

--- .history/scripts/niah_test_20250905122823.sh
+++ .history/scripts/niah_test_20250905122823.sh
@@ -1,19 +1,18 @@
-TOKENIZER_PATH="/mnt/localssd/zefan/TTT/flame_mini/fla-hub-local/transformer-1.3B-100B"  # Change this to your tokenizer path
+TOKENIZER_PATH="/mnt/localssd/TTT/flame_mini/fla-hub-local/transformer-1.3B-100B"  # Change this to your tokenizer path
 TOKENIZER_TYPE="hf"
-SAVE_DIR="/mnt/localssd/data"
+SAVE_DIR="/mnt/localssd/RULER/controllable_output"
 
 python scripts/data/synthetic/niah_controllable.py \
     --save_dir ${SAVE_DIR} \
-    --save_name niah_64k_1k_10pos_5samples_essay_words_numbers2 \
+    --save_name niah_128k_1k_5pos_1samples \
     --tokenizer_path ${TOKENIZER_PATH} \
     --tokenizer_type ${TOKENIZER_TYPE} \
-    --test_max_length 64000 \
+    --test_max_length 128000 \
     --length_interval 1000 \
-    --num_depth_positions 10 \
-    --num_samples 5 \
+    --num_depth_positions 5 \
+    --num_samples 1 \
     --tokens_to_generate 64 \
     --type_haystack essay \
     --type_needle_k words \
     --type_needle_v numbers \
-    --num_digits_needle_v=2 \
     --keep_answer_prefix
